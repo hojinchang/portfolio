9ff2241851578ec965dc1cc26715be14e2267dc5
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-04-11 22:29:06 -0700

    Fix header padding in desktop and mobile mode

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
+import { RootState } from "../../store/store";
+
 const Header: FC = () => {
+    const isMobile = useSelector((state: RootState) => state.isMobile.isMobile);
 
     return (
-        <div className="relative flex p-6 bg-neutral-800 z-100">
+        <div className={`relative flex bg-neutral-800 z-100 ${isMobile ? "p-6" : "px-6 pt-10 pb-5"}`}>
             <header className="w-full">
                 <NavLink to="/">
                     <h1 className="text-3xl text-neutral-200 font-semibold"><span>{"< h "}</span><span>{"/>"}</span></h1>
