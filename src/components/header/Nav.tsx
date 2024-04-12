c6c9e2a39b11cec9996114d0d726419869f33679
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-04-12 00:52:57 -0700

    Refractor nav item into separate component

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -2,6 +2,9 @@ import { FC } from "react"
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 
+
+import NavItem from "./NavItem";
+
 const Nav:FC = () => {
     const isMobile = useSelector((state: RootState) => state.isMobile.isMobile);
 
@@ -12,12 +15,7 @@ const Nav:FC = () => {
             <nav className={`absolute z-10 ${isMobile ? "bg-neutral-800 w-full bottom-0 px-2 py-4 border-t border-neutral-700 shadow-top-shadow" : "top-0 p-8"}`}>
                 <ul className="flex justify-center gap-2 3xs:gap-4 2xs:gap-8 xs:gap-12 sm:gap-16 md:gap-6 lg:gap-16">
                     {navItems.map((item, idx) => (
-                        <li key={idx}>
-                            <a href={`#${(item === "tech stack") ? "tech-stack" : item}`} className="nav-link group">
-                                <p className="nav-top-text">{`0${idx}`}</p>
-                                <p className="nav-bottom-text"><span>//</span> {item.toUpperCase()}</p>
-                            </a>
-                        </li>
+                        <NavItem key={idx} idx={idx} item={item}/>
                     ))}
                 </ul>
             </nav>
---
+++ src/components/header/NavItem.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+
+
+interface Props {
+    idx: number,
+    item: string
+}
+
+const NavItem: FC<Props> = ({ idx, item }) => {
+
+    return (
+        <li>
+            <a href={`#${(item === "tech stack") ? "tech-stack" : item}`} className="nav-link group">
+                <p className="nav-top-text">{`0${idx}`}</p>
+                <p className="nav-bottom-text"><span>//</span> {item.toUpperCase()}</p>
+            </a>
+        </li>
+    )
+}
+
+export default NavItem;
