833d2a0f3469bdadc74cb12700b6c4f58d5bdbd4
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-04-16 02:17:26 -0700

    Fix cursor using framer motion

--- src/components/DotCursor.tsx
+++ src/components/DotCursor.tsx
@@ -1,51 +1,81 @@
-import { FC, useState, useEffect, useRef } from  "react";
+import { FC, useState, useEffect } from  "react";
+import { motion } from "framer-motion";
 
 const DotCursor: FC = () => {
-    const dotRef = useRef<HTMLDivElement | null>(null);
-    const haloRef = useRef<HTMLDivElement | null>(null);
+
+    const [cursorPosition, setCursorPosition] = useState<{ x: number, y: number }>({
+        x: 0,
+        y: 0
+    });
+    const [haloPosition, setHaloPosition] = useState<{ x: number, y: number }>({
+        x: 0,
+        y: 0
+    });
 
     const [isHovered, setIsHovered] = useState(false);
 
-    // Move the dot and its halo based on where cursor is
-    const moveDot = (e: MouseEvent) => {
-        if (dotRef.current && haloRef.current) {
-            dotRef.current.style.left = `${e.pageX}px`;
-            dotRef.current.style.top = `${e.pageY}px`;
-            haloRef.current.style.left = `${e.pageX}px`;
-            haloRef.current.style.top = `${e.pageY}px`;
+    // Set the cursor and halo positions on mouse move
+    useEffect(() => {
+        const moveCursor = (e: MouseEvent) => {
+            setCursorPosition({ 
+                x: e.clientX - 8, 
+                y: e.clientY - 8
+            });
+
+            setHaloPosition({ 
+                x: e.clientX - 20, 
+                y: e.clientY - 20
+            });
         }
-    };
+
+        window.addEventListener("mousemove", moveCursor);
+
+        return () => {
+            window.removeEventListener("mousemove", moveCursor);
+        }
+    }, []);
+
 
     // Add event listeners on links and buttons to set a hover state on mouseover/mouseout
     // This creates the effect where the halo only shows on hover state
     useEffect(() => {
         const handleMouseOver = (e: Event) => {
-            if ((e.target as Element).closest('a, button')) {
+            if ((e.target as Element).closest("a, button")) {
                 setIsHovered(true);
             }
         };
 
         const handleMouseOut = (e: Event) => {
-            if ((e.target as Element).closest('a, button')) {
+            if ((e.target as Element).closest("a, button")) {
                 setIsHovered(false);
             }
         };
 
-        document.addEventListener('mouseover', handleMouseOver);
-        document.addEventListener('mouseout', handleMouseOut);
-        document.addEventListener('mousemove', moveDot);
+        document.addEventListener("mouseover", handleMouseOver);
+        document.addEventListener("mouseout", handleMouseOut);
 
         return () => {
-            document.removeEventListener('mouseover', handleMouseOver);
-            document.removeEventListener('mouseout', handleMouseOut);
-            document.removeEventListener('mousemove', moveDot);
+            document.removeEventListener("mouseover", handleMouseOver);
+            document.removeEventListener("mouseout", handleMouseOut);
         };
     }, []);
 
     return (
         <>
-            <div className="dot z-50" ref={ dotRef } />
-            <div className={`halo z-50 ${isHovered ? "hovered" : ""}`} ref={ haloRef } />
+            <motion.div 
+                className="dot z-50" 
+                style={{
+                    translateX: cursorPosition.x,
+                    translateY: cursorPosition.y,
+                }} 
+            />
+            <motion.div 
+                className={`halo z-50 ${isHovered ? "hovered" : ""}`}  
+                style={{
+                    translateX: haloPosition.x,
+                    translateY: haloPosition.y,
+                }} 
+            />
         </>
     );
 };
