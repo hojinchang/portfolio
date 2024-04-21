aa67176fe36c76288710f9346ee3db4e00afd8d4
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-04-21 15:40:30 -0700

    Add stagger animation to project articles

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useRef } from "react"
 import { useSelector } from "react-redux";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
@@ -8,11 +8,12 @@ import NavItem from "./NavItem";
 
 const Nav:FC = () => {
     const isMobile = useSelector((state: RootState) => state.isMobile.isMobile);
+    const navRef = useRef<HTMLElement>(null);
 
     const navItems: string[] = ["home", "projects", "about", "tech stack", "contact"];
     
     useGSAP(() => {
-        gsap.from(".nav", {
+        gsap.from(navRef.current, {
             opacity: 0,
             y: -20,
             duration: 1,
@@ -22,7 +23,7 @@ const Nav:FC = () => {
 
     return (
         <div className="flex justify-center">
-            <nav className={`nav mx-auto w-content z-10 ${isMobile ? "fixed left-0 right-0 bottom-0 px-2 py-4 bg-neutral-900 border-t border-neutral-700 shadow-top-shadow" : "absolute top-0 px-8 pt-6"}`}>
+            <nav ref={ navRef } className={ `mx-auto w-content z-10 ${isMobile ? "fixed left-0 right-0 bottom-0 px-2 py-4 bg-neutral-900 border-t border-neutral-700 shadow-top-shadow" : "absolute top-0 px-8 pt-6"}` }>
                 <ul className="flex justify-center gap-2 3xs:gap-4 2xs:gap-8 xs:gap-12 sm:gap-16 md:gap-6 lg:gap-16">
                     {navItems.map(( item, idx ) => (
                         <NavItem key={ idx } idx={ idx } item={ item }/>
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,6 +1,8 @@
-import { FC, useState, useEffect } from "react";
+import React, { FC, useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+
+import gsap from "gsap";
 
 import Header from "../components/header/Header";
 import ProjectArticle from "../components/project_articles/ProjectArticle";
@@ -15,20 +17,29 @@ import { projectsAPIPath } from "../global/wpAPIPath";
 const ProjectsPage: FC = () => {
     const isMobile = useSelector(( state: RootState ) => state.isMobile.isMobile);
     const [loading, setLoading] = useState<boolean>(true);
+
     const [projects, setProjects] = useState<ProjectInterface[]>([]);
 
+    // References for GSAP animation
+    const titleRef = useRef<HTMLHeadingElement>(null);
+    const descriptionRef = useRef<HTMLParagraphElement>(null);
+    // const projectRefs = useRef<HTMLDivElement[]>([]);
+    const projectRefs = useRef<Array<React.RefObject<HTMLDivElement>>>([]);
 
     // Scroll to the top of the page when the page mounts
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
+    // Fetch the projects
     useEffect(() => {
         const fetchProjects = async() => {
             try {
                 const response = await axios.get(projectsAPIPath);
                 setProjects(response.data);
-                // setLoading(false);
+                projectRefs.current = response.data.map(() => React.createRef<HTMLDivElement>());
+
+                console.log(projectRefs);
             } catch(err) {
                 console.error("Error fetching projects:", err);
                 setLoading(false);
@@ -36,13 +47,43 @@ const ProjectsPage: FC = () => {
         }
         fetchProjects();
 
+        // Set loading timer
         const timer = setTimeout(() => {
             setLoading(false);
-        }, 1500); // Minimum loading time of 1 second
+        }, 1500);
 
         return () => clearTimeout(timer);
-    }, [])
+    }, []);
 
+    // Apply animation after loading is completed
+    useEffect(() => {
+        if (!loading && projects.length) {
+            gsap.from(titleRef.current, {
+                opacity: 0,
+                y: -20,
+                duration: 1,
+                ease: "power1.inOut"
+            });
+    
+            gsap.from(descriptionRef.current, {
+                opacity: 0,
+                y: 20,
+                duration: 1,
+                ease: "power1.inOut"
+            });
+
+            gsap.from(projectRefs.current.map(ref => ref.current), {
+                opacity: 0,
+                y: 30,
+                stagger: 0.3,
+                duration: 1,
+                ease: "power1.out"
+            });
+        }
+        
+    }, [loading, projects.length]); 
+
+    // Show loading animation
     if (loading) {
         return (
             <main>
@@ -56,14 +97,16 @@ const ProjectsPage: FC = () => {
             <Header />
             <main className={ `px-4 ${ isMobile ? "pb-20" : "" }` }>
                 <section className="max-w-[1400px] mx-auto mt-28 mb-16">
-                    <h1 className="font-bold text-center mb-12 text-5xl 2xs:text-6xl sm:text-7xl lg:text-8xl xl:text-9xl">PROJECTS</h1>
-                    <p className="text-center text-neutral-400">Here is a showcase of all of my projects.</p>
+                    <h1 ref={ titleRef } className="font-bold text-center mb-12 text-5xl 2xs:text-6xl sm:text-7xl lg:text-8xl xl:text-9xl">PROJECTS</h1>
+                    <p ref={ descriptionRef } className="text-center text-neutral-400">Here is a showcase of all of my projects.</p>
                 </section>
                 <section className="max-w-5xl mx-auto">
                     <div className="mx-auto grid gap-4 grid-cols-1 max-w-[400px] sm:max-w-none sm:grid-cols-2 lg:grid-cols-3">
                         {projects.length > 0 && (
-                            projects.map(( project ) => (
-                                <ProjectArticle key={ project.title.rendered } project={ project } />
+                            projects.map(( project, idx ) => (
+                                <div ref={ projectRefs.current[idx]} key={ project.title.rendered }>
+                                    <ProjectArticle project={ project } />
+                                </div>
                             ))
                         )}
                     </div>
